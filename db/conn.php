932d9cf6416adade2085df4e80e0a159a6bc266f
Jordonwill25 <jordonwilliams2539@gmail.com>
2022-12-21 11:13:00 -0500

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,5 +1,6 @@
 
 <?php 
+/*
     $host= '127.0.0.1';
     $port='3307';
     $db='footballform';
@@ -31,5 +32,38 @@
 
     $user->insertUser("admin", "dministrat0r");
    
-    
+    */
+?>
+
+
+<?php  
+    $host= 'applied-web.mysql.database.azure.com';
+    $db='jordonwilliams_assignment3';
+    $user='appliedweb_user@applied-web';
+    $pass='P@ssword1';
+    $charset='utf8mb4';
+
+
+    $dsn="mysql:host=$host;dbname=$db;charset=$charset";
+
+    try{
+
+        $pdo= new PDO($dsn,$user,$pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo'hello database';
+
+    }catch(PDOException $e){
+            
+        echo "<h1 class ='text-danger'>No data found</h1>";
+       // throw new PDOException($e ->getMessage());
+
+    };
+
+    require_once "crud.php";
+    require_once "user.php";
+    $crud = new crud($pdo);
+    $user = new user($pdo);
+
+    $user->insertUser("admin", "dministrat0r");
+
 ?>
